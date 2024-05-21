6ad2c869c0063d673aeacd89625400d4fd905086
x9iec <gsinu0607@gmail.com>
2024-05-21 20:00:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
-import discord
-from discord.ext import commands
 
-client = commands.Bot(intents=discord.Intents.all(), command_prefix='!')
-token = 'MTI0MjQyOTYxNzIyNzMwMDkzNA.G1g2Yc.YFRZ52x_36WBdRxCAKzcWdsDuujSjDUV-6HDeY'
-
-@client.event
-async def on_ready():
-    await client.tree.sync()
-
-@client.command(name="청소")
-async def clear(ctx, amount: int):
-    try:
-        await ctx.channel.purge(limit=amount+1)
-        await ctx.send(f"{amount}개의 메시지가 삭제되었습니다.")
-    except ValueError:
-        await ctx.send("숫자로 된 수량을 입력하세요.")
-
-client.run(token)
